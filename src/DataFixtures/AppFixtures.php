5c5bda8a66b4eace7a3e753e980f2a48e6b04a91
AliciaFitzz <falonne34@outlook.com>
2023-05-29 16:09:31 +0200

    hashage MDP

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,10 +4,12 @@ namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
 use App\Entity\Recipe;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Faker\Generator;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -15,6 +17,7 @@ class AppFixtures extends Fixture
      * @var Generator
      */
     private Generator $faker;
+
 
     public function __construct()
     {
@@ -55,6 +58,18 @@ class AppFixtures extends Fixture
             }
 
             $manager->persist($recipe);
+        }
+
+        // USER
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPaswword('password');
+
+            $manager->persist($user);
         }
 
         $manager->flush();
