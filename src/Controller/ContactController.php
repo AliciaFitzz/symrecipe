c63c05fa2dca8eb5b04522c5b565104b42631ec3
AliciaFitzz <falonne34@outlook.com>
2023-09-20 16:33:09 +0200

    [add] envoie d'email à la soumission du formulaire de contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,18 +5,21 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact_index')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
 
+        // Si l'utilisateur existe, je remplie directement ses informations
         if ($this->getUser()) {
             $contact->setFullName($this->getUser()->getFullName())
                 ->setEmail($this->getUser()->getEmail());
@@ -31,6 +34,20 @@ class ContactController extends AbstractController
             $manager->persist($contact);
             $manager->flush();
 
+            // Email
+            $email = (new TemplatedEmail()) // Créer un template pour les emails
+                ->from($contact->getEmail())
+                ->to('admin@symrecipe.com')
+                ->subject($contact->getSubject())
+                ->htmlTemplate('emails/contact.html.twig') // Renvoie vers notre template
+
+                ->context([
+                    'contact' => $contact
+                ]);
+
+            $mailer->send($email);
+
+            // Message de succès
             $this->addFlash(
                 'success',
                 'Votre message a bien été envoyé.'
