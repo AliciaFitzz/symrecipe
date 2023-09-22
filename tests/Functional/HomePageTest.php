e4ff7b0721dbf33ce9d477118d8d22e082269ed7
AliciaFitzz <falonne34@outlook.com>
2023-09-22 16:12:05 +0200

    [add] test fonctionel sur la page d'accueil

---
+++ tests/Functional/HomePageTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomePageTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+
+        // vérifie s'il y a bien un éléments avec les classes mentionnée
+        $button = $crawler->filter('.btn .btn-primary .btn-lgz');
+        $this->assertEquals(1, count($button)); // on en attend 1
+
+        $recipes = $crawler->filter('.recipes .card');
+        $this->assertEquals(3, count($recipes));
+
+        $this->assertSelectorTextContains('h1', 'Bienvenue sur SymRecipe');
+    }
+}
